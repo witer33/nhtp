f2919c9d20116ec12ba1c55ff77e6e9866dba18f
witer33 <leox337@gmail.com>
2020-11-02 20:00:29 +0100

    Update find.nim

--- examples/find.nim
+++ examples/find.nim
@@ -2,4 +2,6 @@ import engine, httpclient, strtabs
 
 var html = Engine(html: newHttpClient().getContent("http://example.com"))
 
+echo html.find("title").getAllText()
+
 echo html.find("meta", args={"http-equiv": "Content-type"}.newStringTable).args["content"]
